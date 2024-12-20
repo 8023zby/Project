85397d412ae61d061923f6470e9ffe44ae5332cc
zhangbingyan <zhangbingyan@yarward.com>
2024-12-20 14:43:05 +0800

    图片切割上传

---
+++ Project/图片切割上传/index.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <v-upload-image :imageObj="imageObj.doorQrCode"></v-upload-image>
+  </div>
+</template>
+
+<script>
+import vUploadImage from "./CropperUploadImg.vue";
+export default {
+  components: { vUploadImage },
+  data () {
+    return {
+			imageObj: {
+        doorQrCode: {
+					imglable: "",
+          avatar: "avatar",
+          imgType: "oldman",
+          imageUrl: ""
+				}
+			},
+    };
+  },
+
+  async mounted () {
+  },
+  watch: {
+  },
+  methods: {}
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
